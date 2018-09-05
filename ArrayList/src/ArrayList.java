1f088025ffd17582818d6b3e7812c55e8ec63df9
Morgan <mmyhill20@kentdenver.org>
2018-09-05 14:24:15 -0600

    Fixed a portion of private get Node method

--- ArrayList/src/ArrayList.java
+++ ArrayList/src/ArrayList.java
@@ -1,3 +1,4 @@
+
 /**
  * Name: Izzy Hurley
  * Lab Name: ArrayList
@@ -11,20 +12,38 @@ public class ArrayList {
     Node front;
     int size;
 
+    public ArrayList(Node front){
+        this.front = front;
+        size = 1;
+    }
+
     public int size(){return size;}
 
-    public void add(int i, Object o){
+    public void add(int i, Object o){//pointing to the node "behind"
         Node n= new Node(o);
         n.pointMe(getNode(i-1));
         getNode(i+1).pointMe(n);
+        size++;
     }
+
+    public void add(Object o){
+        Node n= new Node(o);
+        n.pointMe(getNode(size-1));//"direction" of pointer is irrelevant
+        size++;
+    }
+
     private Node getNode(int i){//if i is the index of the node being accessed
         if(i >= size|| i < 0){return null;}//needs to be >= size, bc ind = size is undefined
-        Node n= front.getPointer(0);
-        for(int q=0;q<=i;q++){//also this needs to start at 0 bc you will reach the index before the index given
+        Node n= front;//needs to be front rather than front.getPointer() for if size is one
+        for(int q=1; q<=i; q++){//also this needs to start at 0 bc you will reach the index before the index given
             n=n.getPointer(0);
         }
     return n;
+    }
+
+
+    public Object get(int i){
+        return getNode(i).getData();
     }
 
     public Object set(int ind, Object O){
